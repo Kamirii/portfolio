4121cdaa392656b41dda4a809236f0730269debe
Kamirii <camilyaalbres@gmail.com>
2022-06-19 00:08:30 -0300

    first version

---
+++ main.js
@@ -0,0 +1,10 @@
+function darkMode() {
+    var element = document.body;
+    element.className = "dark-mode";
+
+  }
+  function lightMode() {
+    var element = document.body;
+    element.className = "light-mode";
+   
+  }
